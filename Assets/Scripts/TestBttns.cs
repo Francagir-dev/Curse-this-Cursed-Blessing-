6eba8cb58362cddee6e905bcf2243b9b74bab14a
Francisco Carrasco Girbés <60694098+Francagir-dev@users.noreply.github.com>
2021-12-02 12:43:06 +0100

    Added UI navigation to new input system
    Added a small scene as well to test

---
+++ Assets/Scripts/TestBttns.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBttns : MonoBehaviour
+{
+    public void ClickBTN()
+    {
+        Debug.Log("Pressed");
+    }
+}
