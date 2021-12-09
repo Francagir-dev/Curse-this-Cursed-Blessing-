4423e81ef483ebf57d3f2394d0a5b31716636c86
Francisco Carrasco Girbés <francagir@hotmail.com>
2021-12-09 15:21:46 +0100

    Added localization and more input map

---
+++ Assets/Scripts/MANAGERS/SingletonPattern.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace Managers
+{
+    public class SingletonPattern<T> : MonoBehaviour where T : Component
+    {
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = FindObjectOfType<T>();
+                    if (_instance == null)
+                    {
+                        GameObject obj = new GameObject(typeof(T).Name);
+                        _instance = obj.AddComponent<T>();
+                    }
+                }
+
+                return _instance;
+            }
+        }
+    }
+}
